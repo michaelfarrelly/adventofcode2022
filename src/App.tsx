62e5b6e3611d28d08d0148ed32597b574ebd4b2e
Michael Farrelly <mikefarrelly@gmail.com>
2022-12-02 12:14:36 -0700

    Day 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Day1 } from "./Day1";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [day, setDay] = useState<number>(0);
 
   return (
     <div className="App">
@@ -17,18 +19,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={() => setDay(1)}>Day 1</button>
+        {day === 1 && <Day1></Day1>}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
