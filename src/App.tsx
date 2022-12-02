a24eb5744cadc4b75321e3d23ee3a75cc62d41a8
Michael Farrelly <mikefarrelly@gmail.com>
2022-12-02 13:49:44 -0700

    Day 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Day1 } from "./Day1";
+import { Day2 } from "./Day2";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,18 +10,17 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Advent of Code 2022</h1>
       <div className="card">
-        <button onClick={() => setDay(1)}>Day 1</button>
-        {day === 1 && <Day1></Day1>}
+        {day == 0 && (
+          <>
+            <button onClick={() => setDay(1)}>Day 1</button>
+            <button onClick={() => setDay(2)}>Day 2</button>
+          </>
+        )}
+        {day > 0 && <button onClick={() => setDay(0)}>Reset day</button>}
+        {day === 1 && <Day1 />}
+        {day === 2 && <Day2 />}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
