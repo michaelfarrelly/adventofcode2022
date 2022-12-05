0d407dcc86606d1b84a8bb727f7d1187e6924fb4
Michael Farrelly <mikefarrelly@gmail.com>
2022-12-05 11:37:38 -0700

    Pre fill day components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import { useCallback, useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Day1 } from "./Day1";
+import { Day10 } from "./Day10";
+import { Day11 } from "./Day11";
+import { Day12 } from "./Day12";
+import { Day13 } from "./Day13";
+import { Day14 } from "./Day14";
+import { Day15 } from "./Day15";
+import { Day16 } from "./Day16";
+import { Day17 } from "./Day17";
+import { Day18 } from "./Day18";
+import { Day19 } from "./Day19";
 import { Day2 } from "./Day2";
+import { Day20 } from "./Day20";
+import { Day21 } from "./Day21";
+import { Day22 } from "./Day22";
+import { Day23 } from "./Day23";
+import { Day24 } from "./Day24";
+import { Day25 } from "./Day25";
 import { Day3 } from "./Day3";
 import { Day4 } from "./Day4";
 import { Day5 } from "./Day5";
+import { Day6 } from "./Day6";
+import { Day7 } from "./Day7";
+import { Day8 } from "./Day8";
+import { Day9 } from "./Day9";
 
 const range = (start: number, end: number): number[] => {
   const items: number[] = [];
@@ -16,7 +35,6 @@ const range = (start: number, end: number): number[] => {
 };
 
 function App() {
-  const [count, setCount] = useState(0);
   const [day, setDay] = useState<number>(0);
   const dayChanged = useCallback((value: string) => {
     setDay(parseInt(value));
@@ -38,6 +56,26 @@ function App() {
         {day === 3 && <Day3 />}
         {day === 4 && <Day4 />}
         {day === 5 && <Day5 />}
+        {day === 6 && <Day6 />}
+        {day === 7 && <Day7 />}
+        {day === 8 && <Day8 />}
+        {day === 9 && <Day9 />}
+        {day === 10 && <Day10 />}
+        {day === 11 && <Day11 />}
+        {day === 12 && <Day12 />}
+        {day === 13 && <Day13 />}
+        {day === 14 && <Day14 />}
+        {day === 15 && <Day15 />}
+        {day === 16 && <Day16 />}
+        {day === 17 && <Day17 />}
+        {day === 18 && <Day18 />}
+        {day === 19 && <Day19 />}
+        {day === 20 && <Day20 />}
+        {day === 21 && <Day21 />}
+        {day === 22 && <Day22 />}
+        {day === 23 && <Day23 />}
+        {day === 24 && <Day24 />}
+        {day === 25 && <Day25 />}
       </div>
     </div>
   );
---
+++ src/Day21.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import "./App.css";
+import styles from "./App.module.css";
+import React from "react";
+/*
+
+Description goes here
+
+*/
+
+const testData =
+  "    [D]    \n[N] [C]    \n[Z] [M] [P]\n 1   2   3 \n\nmove 1 from 2 to 1\nmove 3 from 1 to 3\nmove 2 from 2 to 1\nmove 1 from 1 to 2";
+
+export function Day21() {
+  const [input, setInput] = useState<string>(testData);
+  const [result, setResult] = useState<string>("");
+
+  const onRunPart1 = React.useCallback(() => {
+    const lines = input.split("\n");
+
+    setResult(`Total Score: ${lines}`);
+  }, [input]);
+
+  const onRunPart2 = React.useCallback(() => {
+    const lines = input.split("\n");
+
+    setResult(`Total Score: ${lines}`);
+  }, [input]);
+
+  return (
+    <>
+      <div className="App">
+        <h1>Day 6</h1>
+      </div>
+
+      <div className={styles.dayGrid}>
+        <div className={styles.dayButtons}>
+          <button onClick={onRunPart1}>Run (Part 1)</button>
+          <button onClick={onRunPart2}>Run (Part 2)</button>
+        </div>
+        <div className={styles.dayInput}>
+          <textarea
+            value={input}
+            rows={10}
+            onChange={(e) => setInput(e.currentTarget.value)}
+          ></textarea>
+        </div>
+        <div className={styles.dayResult}>
+          <textarea value={result} rows={10}></textarea>
+        </div>
+      </div>
+    </>
+  );
+}
