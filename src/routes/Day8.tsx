ad8e5b808f258fb5f0759739681dc7966b3f93d3
Michael Farrelly <mikefarrelly@gmail.com>
2022-12-09 16:32:21 -0700

    Day 8

--- src/routes/Day8.tsx
+++ src/routes/Day8.tsx
@@ -4,12 +4,250 @@ import styles from "./App.module.css";
 import React from "react";
 /*
 
-Description goes here
+--- Day 8: Treetop Tree House ---
+
+The expedition comes across a peculiar patch of tall trees all planted carefully in a grid. The Elves explain that a previous expedition planted these trees as a reforestation effort. Now, they're curious if this would be a good location for a tree house.
+
+First, determine whether there is enough tree cover here to keep a tree house hidden. To do this, you need to count the number of trees that are visible from outside the grid when looking directly along a row or column.
+
+The Elves have already launched a quadcopter to generate a map with the height of each tree (your puzzle input). For example:
+
+30373
+25512
+65332
+33549
+35390
+
+Each tree is represented as a single digit whose value is its height, where 0 is the shortest and 9 is the tallest.
+
+A tree is visible if all of the other trees between it and an edge of the grid are shorter than it. Only consider trees in the same row or column; that is, only look up, down, left, or right from any given tree.
+
+All of the trees around the edge of the grid are visible - since they are already on the edge, there are no trees to block the view. In this example, that only leaves the interior nine trees to consider:
+
+    The top-left 5 is visible from the left and top. (It isn't visible from the right or bottom since other trees of height 5 are in the way.)
+    The top-middle 5 is visible from the top and right.
+    The top-right 1 is not visible from any direction; for it to be visible, there would need to only be trees of height 0 between it and an edge.
+    The left-middle 5 is visible, but only from the right.
+    The center 3 is not visible from any direction; for it to be visible, there would need to be only trees of at most height 2 between it and an edge.
+    The right-middle 3 is visible from the right.
+    In the bottom row, the middle 5 is visible, but the 3 and 4 are not.
+
+With 16 trees visible on the edge and another 5 visible in the interior, a total of 21 trees are visible in this arrangement.
+
+Consider your map; how many trees are visible from outside the grid?
+
+
+--- Part Two ---
+
+Content with the amount of tree cover available, the Elves just need to know the best spot to build their tree house: they would like to be able to see a lot of trees.
+
+To measure the viewing distance from a given tree, look up, down, left, and right from that tree; stop if you reach an edge or at the first tree that is the same height or taller than the tree under consideration. (If a tree is right on the edge, at least one of its viewing distances will be zero.)
+
+The Elves don't care about distant trees taller than those found by the rules above; the proposed tree house has large eaves to keep it dry, so they wouldn't be able to see higher than the tree house anyway.
+
+In the example above, consider the middle 5 in the second row:
+
+30373
+25512
+65332
+33549
+35390
+
+    Looking up, its view is not blocked; it can see 1 tree (of height 3).
+    Looking left, its view is blocked immediately; it can see only 1 tree (of height 5, right next to it).
+    Looking right, its view is not blocked; it can see 2 trees.
+    Looking down, its view is blocked eventually; it can see 2 trees (one of height 3, then the tree of height 5 that blocks its view).
+
+A tree's scenic score is found by multiplying together its viewing distance in each of the four directions. For this tree, this is 4 (found by multiplying 1 * 1 * 2 * 2).
+
+However, you can do even better: consider the tree of height 5 in the middle of the fourth row:
+
+30373
+25512
+65332
+33549
+35390
+
+    Looking up, its view is blocked at 2 trees (by another tree with a height of 5).
+    Looking left, its view is not blocked; it can see 2 trees.
+    Looking down, its view is also not blocked; it can see 1 tree.
+    Looking right, its view is blocked at 2 trees (by a massive tree of height 9).
+
+This tree's scenic score is 8 (2 * 2 * 1 * 2); this is the ideal spot for the tree house.
+
+Consider each tree on your map. What is the highest scenic score possible for any tree?
+
+
 
 */
+interface Tree {
+    height: number;
+}
 
-const testData =
-    "    [D]    \n[N] [C]    \n[Z] [M] [P]\n 1   2   3 \n\nmove 1 from 2 to 1\nmove 3 from 1 to 3\nmove 2 from 2 to 1\nmove 1 from 1 to 2";
+interface Row {
+    trees: Tree[];
+}
+
+interface Column {
+    trees: Tree[];
+}
+
+function buildColumn(rows: Row[], columnIndex: number): Column {
+    const trees: Tree[] = [];
+    trees.push(
+        ...rows.map(r => {
+            return r.trees[columnIndex];
+        })
+    );
+    return { trees };
+}
+
+function tallestHeight(row: Row, col: Column): { col: number; row: number } {
+    const rowHeight = row.trees
+        .map((item, i) => {
+            return { item, index: i };
+        })
+        .reduce((prev, curr) => {
+            if (curr.item.height > prev) {
+                return curr.item.height;
+            }
+            return prev;
+        }, 0);
+    const colHeight = col.trees
+        .map((item, i) => {
+            return { item, index: i };
+        })
+        .reduce((prev, curr) => {
+            if (curr.item.height > prev) {
+                return curr.item.height;
+            }
+            return prev;
+        }, 0);
+    return { col: colHeight, row: rowHeight };
+}
+
+// function tallestAndVisibleTree(row: Row, col: Column, x: number, y: number): boolean {
+//     const maxHeight = tallestHeight(row, col);
+//     for (const t of row.trees) {
+//         isTallestHeight(row);
+//     }
+//     return false;
+// }
+
+function buildRow(line: string): Row {
+    const trees: Tree[] = [];
+    for (const s of line) {
+        trees.push({ height: parseInt(s) });
+    }
+    return { trees };
+}
+
+class Model {
+    constructor(rows: Row[]) {
+        this.rows = rows;
+    }
+
+    getRowAndColAt(x: number, y: number): { left: Tree[]; right: Tree[]; up: Tree[]; down: Tree[] } {
+        const size = { x: this.rows[0].trees.length, y: this.rows.length };
+        const row = this.rows[y];
+        const col = buildColumn(this.rows, x);
+
+        const left = x > 0 ? row.trees.slice(0, x) : [];
+        const right = x < size.x ? row.trees.slice(x + 1) : [];
+        const up = y > 0 ? col.trees.slice(0, y) : [];
+        const down = y < size.y ? col.trees.slice(y + 1) : [];
+        return { left, right, up, down };
+    }
+
+    findTallestTrees(): Tree[] {
+        const trees: Tree[] = [];
+        const size = { x: this.rows[0].trees.length, y: this.rows.length };
+        for (let i = 0; i < size.y; i++) {
+            for (let j = 0; j < size.x; j++) {
+                if (i === 0 || j === 0 || i === size.y - 1 || j === size.x - 1) {
+                    // always visible from "outside"
+                    // window.console.log(`always`, { i, j });
+                    trees.push(this.rows[i].trees[j]);
+                    continue;
+                } else {
+                    const treesAround = this.getRowAndColAt(j, i);
+                    const currentHeight = this.rows[i].trees[j].height;
+                    const tallestLeft = treesAround.left.every(v => v.height < currentHeight);
+                    const tallestRight = treesAround.right.every(v => v.height < currentHeight);
+                    const tallestUp = treesAround.up.every(v => v.height < currentHeight);
+                    const tallestDown = treesAround.down.every(v => v.height < currentHeight);
+                    window.console.log(`x`, {
+                        i,
+                        j,
+                        currentHeight,
+                        treesAround,
+                        added: tallestLeft || tallestRight || tallestDown || tallestUp
+                    });
+                    if (tallestLeft || tallestRight || tallestDown || tallestUp) {
+                        // window.console.log(`x`, { i, j, currentHeight, treesAround });
+                        trees.push(this.rows[i].trees[j]);
+                    }
+                }
+            }
+        }
+        return trees;
+    }
+
+    findViewingDistnace(items: Tree[], currentHeight: number): number {
+        for (let i = 0; i < items.length; i++) {
+            if (items[i].height >= currentHeight) {
+                return i + 1;
+            }
+        }
+        return items.length;
+    }
+
+    findBestViewingDistanceScore(): number {
+        const trees: { tree: Tree; vd: number }[] = [];
+        const size = { x: this.rows[0].trees.length, y: this.rows.length };
+        for (let i = 0; i < size.y; i++) {
+            for (let j = 0; j < size.x; j++) {
+                const treesAround = this.getRowAndColAt(j, i);
+                const currentHeight = this.rows[i].trees[j].height;
+                const tallestLeft =
+                    treesAround.left.length > 0
+                        ? this.findViewingDistnace(treesAround.left.reverse(), currentHeight)
+                        : 0;
+                const tallestRight =
+                    treesAround.right.length > 0 ? this.findViewingDistnace(treesAround.right, currentHeight) : 0;
+                const tallestUp =
+                    treesAround.up.length > 0 ? this.findViewingDistnace(treesAround.up.reverse(), currentHeight) : 0;
+                const tallestDown =
+                    treesAround.down.length > 0 ? this.findViewingDistnace(treesAround.down, currentHeight) : 0;
+                window.console.log(`x`, {
+                    i,
+                    j,
+                    currentHeight,
+                    treesAround,
+                    added: tallestLeft * tallestRight * tallestDown * tallestUp
+                });
+                // if (tallestLeft || tallestRight || tallestDown || tallestUp) {
+                // window.console.log(`x`, { i, j, currentHeight, treesAround });
+                trees.push({ tree: this.rows[i].trees[j], vd: tallestLeft * tallestRight * tallestDown * tallestUp });
+                // }
+            }
+        }
+        return trees.reduce((prev, curr) => {
+            if (curr.vd > prev) {
+                return curr.vd;
+            }
+            return prev;
+        }, 0);
+    }
+    rows: Row[];
+}
+
+function buildModel(lines: string[]): Model {
+    const rows = lines.map(l => buildRow(l));
+    return new Model(rows);
+}
+
+const testData = "30373\n25512\n65332\n33549\n35390";
 
 export function Day8() {
     const [input, setInput] = useState<string>(testData);
@@ -17,14 +255,18 @@ export function Day8() {
 
     const onRunPart1 = React.useCallback(() => {
         const lines = input.split("\n");
-
-        setResult(`Total Score: ${lines}`);
+        const model = buildModel(lines);
+        const trees = model.findTallestTrees();
+        window.console.log(`result`, { trees });
+        setResult(`Total Score: ${trees.length}`);
     }, [input]);
 
     const onRunPart2 = React.useCallback(() => {
         const lines = input.split("\n");
-
-        setResult(`Total Score: ${lines}`);
+        const model = buildModel(lines);
+        const trees = model.findBestViewingDistanceScore();
+        window.console.log(`result`, { trees });
+        setResult(`Total Score: ${trees}`);
     }, [input]);
 
     return (
